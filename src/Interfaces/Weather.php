3ca4b715b0704b236660e36450725f7160697a32
Marcin Puszka <marcin.puszka@coditive.co>
2022-03-09 21:27:01 +0100

    Add missing function to interface

--- src/Interfaces/Weather.php
+++ src/Interfaces/Weather.php
@@ -4,6 +4,8 @@ namespace App\Interfaces;
 
 interface Weather
 {
+  public function makeRequest(object $requestService, string $city, string $country): object;
+
   public function prepareQuery(string $city, string $country);
 
   public function getLocationName();
