d7149b75c8055478c44db6b9a23d41b93ec3469f
Marcin Puszka <marcin.puszka@coditive.co>
2022-03-11 13:14:08 +0100

    Add loader to vue components

--- assets/js/front/components/article/components/allArticles.vue
+++ assets/js/front/components/article/components/allArticles.vue
@@ -2,6 +2,12 @@
   <div>
     <h2>{{ title }}</h2>
 
+    <div class="d-flex justify-content-center" v-if="loader">
+      <div class="spinner-border" role="status">
+        <span class="sr-only"></span>
+      </div>
+    </div>
+
     <ul>
       <li v-for="article in articles" :key="article.id">
         <a :href="'/article/' + article.id">
@@ -9,6 +15,10 @@
         </a>
       </li>
     </ul>
+
+    <div v-if="error">
+        No article found
+      </div>
   </div>
 </template>
 
@@ -22,6 +32,8 @@ export default {
       articles: [],
       title: this.$parent.title,
       skip: this.$parent.skip,
+      error: false,
+      loader: true,
     };
   },
   mounted() {
@@ -32,9 +44,13 @@ export default {
         if (this.skip) {
           this.articles = this.articles.filter(item => item.id != this.skip);
         }
+
+        this.loader = false;
       }
     })
     .catch((e) => {
+      this.error = true;
+      this.loader = false;
       console.log(e);
     });
   },
